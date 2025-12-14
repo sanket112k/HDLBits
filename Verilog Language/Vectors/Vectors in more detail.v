6f61b42932aa637dea3bc650a1e86f8aa3a2ae67
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2025-12-14 09:18:35 +0530

    Update Vectors in more detail.v

--- Verilog Language/Vectors/Vectors in more detail.v
+++ Verilog Language/Vectors/Vectors in more detail.v
@@ -1,8 +1,12 @@
+/*Vectors are used to group related signals using one name to make it more convenient to manipulate.
+For example, wire [7:0] w; declares an 8-bit vector named w that is equivalent to having 8 separate wires.
+*/
 `default_nettype none     // Disable implicit nets. Reduces some types of bugs.
 module top_module( 
     input wire [15:0] in,
     output wire [7:0] out_hi,
-    output wire [7:0] out_lo );
+    output wire [7:0] out_lo 
+);
     assign out_hi = in[15:8];
     assign out_lo = in[7:0];
 endmodule
