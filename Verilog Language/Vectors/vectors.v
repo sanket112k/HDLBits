01ec539986b86291c89c6cb0b633d7b63e53af7d
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2025-12-14 09:20:53 +0530

    Update vectors.v

--- Verilog Language/Vectors/vectors.v
+++ Verilog Language/Vectors/vectors.v
@@ -1,3 +1,7 @@
+/* Build a circuit that has one 3-bit input, then outputs the same vector, and also splits it into three separate 1-bit outputs.
+Connect output o0 to the input vector's position 0, o1 to position 1, etc.
+In a diagram, a tick mark with a number next to it indicates the width of the vector (or "bus"), rather than drawing a separate line for each bit in the vector.
+*/
 module top_module ( 
     input wire [2:0] vec,
     output wire [2:0] outv,
