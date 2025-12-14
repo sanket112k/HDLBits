e77cef17dab12a45b43ce6d11eb631d27322a2be
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2025-12-14 09:16:32 +0530

    Update Vector part select.v

--- Verilog Language/Vectors/Vector part select.v
+++ Verilog Language/Vectors/Vector part select.v
@@ -1,3 +1,7 @@
+/*A 32-bit vector can be viewed as containing 4 bytes (bits [31:24], [23:16], etc.). Build a circuit that will reverse the byte ordering of the 4-byte word.
+
+AaaaaaaaBbbbbbbbCcccccccDddddddd => DdddddddCcccccccBbbbbbbbAaaaaaaa
+*/
 module top_module( 
     input [31:0] in,
     output [31:0] out );
