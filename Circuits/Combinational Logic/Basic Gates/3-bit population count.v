8923e5ac814def1ca73cbc85fb629612fdcc7621
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2025-12-16 10:07:44 +0530

    Create 3-bit population count.v

---
+++ Circuits/Combinational Logic/Basic Gates/3-bit population count.v
@@ -0,0 +1,12 @@
+/* A "population count" circuit counts the number of '1's in an input vector. Build a population count circuit for a 3-bit input vector.
+*/
+module top_module( 
+    input [2:0] in,
+    output [1:0] out
+);
+    integer i;
+    always @(*) begin
+        out = 0;
+        for(i=0;i<3;i=i+1) out = out + in[i];
+    end
+endmodule
