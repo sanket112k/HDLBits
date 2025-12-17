1fd1ec51fe2ddfe12e9ef1b342a67894c2691998
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2025-12-17 10:47:13 +0530

    Create DFF with reset value.v

---
+++ Circuits/Sequential Logic/Latches and Flip-Flop/DFF with reset value.v
@@ -0,0 +1,14 @@
+/*
+Create 8 D flip-flops with active high synchronous reset. The flip-flops must be reset to 0x34 rather than zero. All DFFs should be triggered by the negative edge of clk.
+*/
+module top_module (
+    input clk,
+    input reset,
+    input [7:0] d,
+    output [7:0] q
+);
+    always @(negedge clk) begin
+        if(reset) q<= 'h0x34;
+        else q<=d;
+    end
+endmodule
