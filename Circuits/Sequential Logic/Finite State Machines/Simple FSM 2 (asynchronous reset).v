8002d3fa51b9f06a5e404462a82c538658b9f385
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2025-12-26 11:22:29 +0530

    Create Simple FSM 2 (asynchronous reset).v

---
+++ Circuits/Sequential Logic/Finite State Machines/Simple FSM 2 (asynchronous reset).v
@@ -0,0 +1,31 @@
+/*
+This is a Moore state machine with two states, two inputs, and one output. Implement this state machine.
+
+This exercise is the same as fsm2s, but using asynchronous reset.
+*/
+module top_module(
+    input clk,
+    input areset,    // Asynchronous reset to OFF
+    input j,
+    input k,
+    output out); //  
+
+    parameter OFF=0, ON=1; 
+    reg state, next_state;
+
+    always @(*) begin
+        case(state)		// State transition logic
+            OFF: next_state = j ? ON : OFF;
+            ON: next_state = k ? OFF : ON;
+        endcase
+    end
+
+    always @(posedge clk, posedge areset) begin
+        if (areset) state <= OFF; 		// State flip-flops with asynchronous reset
+        else state <= next_state;
+    end
+
+    // Output logic
+    assign out = (state == ON);
+
+endmodule
