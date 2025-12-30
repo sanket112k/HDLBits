96a7bb025da531c76239210a320ff023317776f6
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2025-12-30 22:15:49 +0530

    Update Lemmings 1.v

--- Circuits/Sequential Logic/Finite State Machines/Lemmings 1.v
+++ Circuits/Sequential Logic/Finite State Machines/Lemmings 1.v
@@ -1,3 +1,10 @@
+/*
+The game Lemmings involves critters with fairly simple brains. So simple that we are going to model it using a finite state machine.
+
+In the Lemmings' 2D world, Lemmings can be in one of two states: walking left or walking right. It will switch directions if it hits an obstacle. In particular, if a Lemming is bumped on the left, it will walk right. If it's bumped on the right, it will walk left. If it's bumped on both sides at the same time, it will still switch directions.
+
+Implement a Moore state machine with two states, two inputs, and one output that models this behaviour.
+*/
 module top_module(
     input clk,
     input areset,    // Freshly brainwashed Lemmings walk left.
