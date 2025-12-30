85cbd0e6d1371e55ef1c0b740bf54d2bb44c81ca
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2025-12-30 22:18:59 +0530

    Update Lemmings 4.v

--- Circuits/Sequential Logic/Finite State Machines/Lemmings 4.v
+++ Circuits/Sequential Logic/Finite State Machines/Lemmings 4.v
@@ -1,3 +1,9 @@
+/*
+Although Lemmings can walk, fall, and dig, Lemmings aren't invulnerable. If a Lemming falls for too long then hits the ground, it can splatter. In particular, if a Lemming falls for more than 20 clock cycles then hits the ground, it will splatter and cease walking, falling, or digging (all 4 outputs become 0), forever (Or until the FSM gets reset). There is no upper limit on how far a Lemming can fall before hitting the ground. Lemmings only splatter when hitting the ground; they do not splatter in mid-air.
+
+Extend your finite state machine to model this behaviour.
+*/
+
 module top_module(
     input clk,
     input areset,    // Freshly brainwashed Lemmings walk left.
