7d25c329dd2aac01beda5ac2283908fb4b8e62e8
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-01-13 15:59:38 +0530

    Create Combinational circuit 5.v

---
+++ Verification: Reading Simulations/Build a circuit from a simulation waveform/Combinational circuit 5.v
@@ -0,0 +1,20 @@
+// This is a combinational circuit. Read the simulation waveforms to determine what the circuit does, then implement it.
+
+module top_module (
+    input [3:0] a,
+    input [3:0] b,
+    input [3:0] c,
+    input [3:0] d,
+    input [3:0] e,
+    output [3:0] q
+);
+    always @(*) begin
+        case(c)
+            4'd0: q = b;
+            4'd1: q = e;
+            4'd2: q = a;
+            4'd3: q = d;
+            default: q = 4'hf;
+        endcase
+    end
+endmodule
