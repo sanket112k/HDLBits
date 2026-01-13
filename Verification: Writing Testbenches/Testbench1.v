5384b22716b6e1e68ef26ad6b0cc565a7e164f2b
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-01-13 17:57:29 +0530

    Create Testbench1.v

---
+++ Verification: Writing Testbenches/Testbench1.v
@@ -0,0 +1,15 @@
+/*
+Create a Verilog testbench that will produce the following waveform for outputs A and B:
+*/
+
+module top_module ( output reg A, output reg B );//
+
+    // generate input patterns here
+    initial begin
+        A = 0; B = 0;
+        #10 A = 1;
+        #5  B = 1;
+        #5  A = 0;
+        #20 B = 0;
+    end
+endmodule
