6efeb3b8fc9020d8910075fb32da01b00c86a406
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-01-13 14:57:53 +0530

    Create NAND.v

---
+++ Verification: Reading Simulations/Finding bugs in code/NAND.v
@@ -0,0 +1,12 @@
+/*
+This three-input NAND gate doesn't work. Fix the bug(s).
+
+You must use the provided 5-input AND gate:
+module andgate ( output out, input a, input b, input c, input d, input e );
+*/
+
+module top_module (input a, input b, input c, output out);
+    wire w;
+    andgate inst1 (w, a, b, c, 1, 1);
+    assign out = ~w;
+endmodule
